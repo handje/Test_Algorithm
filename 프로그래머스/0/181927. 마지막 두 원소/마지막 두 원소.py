dc4256905c46fcb19a44a113cad40480fa906b19
Sonjieun <57988006+handje@users.noreply.github.com>
2024-01-20 02:29:38 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+    last=num_list[len(num_list)-1]
+    pre=num_list[len(num_list)-2]
+
+    if(last>pre): num_list.append(last-pre)
+    else: num_list.append(last*2)
+    
+    return num_list
