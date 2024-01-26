c4d8dbca1bd34acf03436907b82452bb2ddb29c3
Sonjieun <57988006+handje@users.noreply.github.com>
2024-01-26 21:58:41 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+    answer = 0
+    for num in num_list:
+        while num>1:
+            if(num%2):num=(num-1)//2
+            else:num//=2
+            answer+=1
+    return answer
