3b8112073e7bab5829c48e2931969cc0cc40f8e1
Sonjieun <57988006+handje@users.noreply.github.com>
2024-01-28 16:33:12 +0900

    [level 0] Title: 배열 만들기 6, Time: 26.20 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    stk = []
+    for i in range(len(arr)):
+        if(len(stk)==0): stk.append(arr[i])
+        elif(stk[-1]==arr[i]):stk.pop()
+        else:stk.append(arr[i])
+
+    return stk if(len(stk)>0) else [-1]
