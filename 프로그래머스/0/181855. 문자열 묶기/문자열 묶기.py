aa8ca1f51fb07343dacf859a6ce782276472dbe7
Sonjieun <57988006+handje@users.noreply.github.com>
2024-01-29 01:51:51 +0900

    [level 0] Title: 문자열 묶기, Time: 43.80 ms, Memory: 17.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -0,0 +1,9 @@
+def solution(strArr):
+    answer = 0
+    strArr=list(map(len,strArr))
+    strLen={}
+    for arr in strArr:
+        if(str(arr) in strLen.keys()): strLen[str(arr)]+=1
+        else: strLen[str(arr)]=1
+    
+    return max(strLen.values())
