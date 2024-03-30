3313008df7ef645bfe802b563fa6ea8f56fc2603
Sonjieun <57988006+handje@users.noreply.github.com>
2024-03-30 15:46:28 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,8 +1,11 @@
 def solution(a, b, c):
+    sum1=a+b+c
+    sum2=a*a+b*b+c*c
+    sum3=a**3+b**3+c**3
     
-    if(a==b and a==c):
-        return (a+b+c)*(a*a+b*b+c*c)*(a**3+b**3+c**3)
-    if(a==b or a==c or b==c):
-        return (a+b+c)*(a*a+b*b+c*c)
-        
-    return a+b+c
+    if a==b==c and a==c==b:
+        return sum1*sum2*sum3
+    elif a!=b!=c and a!=c!=b:
+        return sum1
+    else:
+        return sum1*sum2
