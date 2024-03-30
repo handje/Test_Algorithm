fdeb7be36e16bdd49d438a00433bcfd1aaaa554b
Sonjieun <57988006+handje@users.noreply.github.com>
2024-03-30 22:06:40 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,5 +1,5 @@
 def solution(my_string, m, c):
     answer = ''
-    for i in range(len(my_string)):
-        if(i%m==c-1):answer+=my_string[i]
+    for i in range(c-1,len(my_string),m):
+        answer+=my_string[i]
     return answer
