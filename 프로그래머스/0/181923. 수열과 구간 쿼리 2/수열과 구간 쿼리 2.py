c26d6e2a0787b7e8e239dbbadd10a192cf48b275
Sonjieun <57988006+handje@users.noreply.github.com>
2024-03-30 15:53:54 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,12 +1,11 @@
 def solution(arr, queries):
-    result=[]
-    
-    for query in queries:
-        min=1000001
-        for i in range(query[0],query[1]+1):
-            if(arr[i]>query[2] and min>arr[i]):min=arr[i]        
-        
-        if(min==1000001): result.append(-1)
-        else: result.append(min)
-        
-    return result
+    answer = []
+    for q in queries:
+        res=1000001
+        for a in arr[q[0]:q[1]+1]:
+            if a>q[2] and res>a:
+                res=a
+        if res==1000001:
+            res=-1
+        answer.append(res)
+    return answer
