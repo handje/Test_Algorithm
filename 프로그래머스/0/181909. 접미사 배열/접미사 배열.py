41dace5c886fdfd7907b919a9a744b6f1db524dd
Sonjieun <57988006+handje@users.noreply.github.com>
2024-03-31 15:48:53 +0900

    [level 0] Title: 접미사 배열, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,5 +1,5 @@
 def solution(my_string):
     answer = []
-    for i in range(0,len(my_string)):
+    for i in range(len(my_string)):
         answer.append(my_string[i:])
     return sorted(answer)
