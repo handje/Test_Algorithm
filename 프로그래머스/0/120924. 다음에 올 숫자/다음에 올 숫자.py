f8638261d0af10f9a1bdea4ec6290af93beaa993
Sonjieun <57988006+handje@users.noreply.github.com>
2024-04-03 22:03:24 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,7 @@
+def solution(common):
+    d1=common[1]-common[0]
+    d2=common[2]-common[1]
+    if d1==d2:
+        return common[-1]+d1
+    else:
+        return common[-1]*common[1]//common[0]
