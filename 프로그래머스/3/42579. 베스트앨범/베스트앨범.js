e09d5200d86d54b73dae04f3a22186cce2edcffb
Sonjieun <57988006+handje@users.noreply.github.com>
2024-07-25 21:35:07 +0900

    [level 3] Title: 베스트앨범, Time: 0.62 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,32 +1,39 @@
 function solution(genres, plays) {
-  const genreCountMap = new Map();
-  const albumsWithIndex = genres.map((genre, idx) => ({
-    genre,
-    playCount: plays[idx],
-    idx,
-  }));
-
-  albumsWithIndex.forEach(({ genre, playCount }) => {
-    genreCountMap.set(genre, (genreCountMap.get(genre) || 0) + playCount);
-  });
-
-  const sortedAlbums = albumsWithIndex.sort((a, b) => {
-    if (a.genre !== b.genre)
-      return genreCountMap.get(b.genre) - genreCountMap.get(a.genre);
-    if (a.playCount !== b.playCount) return b.playCount - a.playCount;
-    return a.idx - b.idx;
-  });
-
-  const selectedAlbums = new Map();
-  const answer = [];
-
-  sortedAlbums.forEach(({ genre, idx }) => {
-    const albumCount = selectedAlbums.get(genre) || 0;
-    if (albumCount < 2) {
-      selectedAlbums.set(genre, albumCount + 1);
-      answer.push(idx);
+    const answer=[]
+    const genreCount=new Map();
+    const music=new Map();
+    
+    for(let i=0;i<genres.length;i++){
+        const gr=genres[i];
+        
+        if(genreCount.has(gr)){
+            genreCount.set(gr,genreCount.get(gr)+plays[i])
+        }else{
+            genreCount.set(gr,plays[i])
+        }
+        
+        if(music.has(gr)){
+            music.set(gr,[...music.get(gr),[i,plays[i]]])
+        }else{
+            music.set(gr,[[i,plays[i]]])
+        }
     }
-  });
-    console.log(genreCountMap,selectedAlbums)
-  return answer;
+    
+    const sortedGenre=Array.from(genreCount.entries()).sort((a,b)=>b[1]-a[1])
+    
+    for(let [key,values] of music){
+        const sorted=Array.from(values).sort((a,b)=>b[1]-a[1]);
+        music.set(key,sorted)
+    }
+
+    for(let g of sortedGenre){
+        const temp=music.get(g[0]);  
+        if(temp.length>=2){
+            answer.push(temp[0][0]);
+         answer.push(temp[1][0]);
+        }else{
+            answer.push(temp[0][0])
+        }
+    }
+    return answer
 }
