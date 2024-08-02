4aaf9d02b0dda2297787e809cdc6babd1a73db9e
Sonjieun <57988006+handje@users.noreply.github.com>
2024-08-02 15:50:36 +0900

    [level 2] Title: 기능개발, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,25 +1,24 @@
 function solution(progresses, speeds) {
     var answer = [];
 
-    const day=[];
-    for(let i=0;i<progresses.length;i++){
-        const last=100-progresses[i]
-        day.push(Math.ceil(last/speeds[i]))
-    }
-
-    
-    let prev=day[0];
-        let count=1;
-    for(let i=1;i<day.length;i++){
-        if(prev<day[i]){
-
-            answer.push(count);
-            count=1;
-            prev=day[i]
-        }else{
-            count++;
+    while(speeds.length > 0) {
+        // 개발
+        for(let i in speeds) {
+            if(progresses[i] < 100) {
+                progresses[i] += speeds[i];
+            }
+        }
+        // 배포
+        let deploy_count = 0;
+        while(progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            deploy_count++;
+        }
+        if(deploy_count > 0) {
+            answer.push(deploy_count);
         }
     }
-    answer.push(count)
+
     return answer;
 }
