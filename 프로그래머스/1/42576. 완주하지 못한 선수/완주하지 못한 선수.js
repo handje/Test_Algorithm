78a83dfbec5604faa831eae434c219afe1175a0e
Sonjieun <57988006+handje@users.noreply.github.com>
2024-10-28 17:18:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.46 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,18 +1,23 @@
 function solution(participant, completion) {
-    const checkMap=new Map();
-    
-    for(let com of completion){
-        if(checkMap.has(com)){
-            checkMap.set(com,checkMap.get(com)+1)
-        }else{
-            checkMap.set(com,1)
-        }
+  const map = new Map();
+
+  for (let p of participant) {
+    if (map.has(p)) {
+      map.set(p, map.get(p) + 1);
+    } else {
+      map.set(p, 1);
     }
-    
-    for(let par of participant){
-        if(!checkMap.has(par)||checkMap.get(par)===0){
-            return par
-        }
-        checkMap.set(par,checkMap.get(par)-1)
+  }
+
+  for (let c of completion) {
+    if (map.has(c)) {
+      map.set(c, map.get(c) - 1);
     }
+  }
+
+  for (let [key, value] of map.entries()) {
+    if (value === 1) {
+      return key;
+    }
+  }
 }
