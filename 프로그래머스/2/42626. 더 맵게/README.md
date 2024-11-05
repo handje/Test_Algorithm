0f6b236a2accbea299cf30024a1f4e5a96415bdb
Sonjieun <57988006+handje@users.noreply.github.com>
2024-11-05 17:47:35 +0900

    [level 2] Title: 더 맵게, Time: 612.10 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 610.53 ms
+메모리: 102 MB, 시간: 612.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 20일 15:40:44
+2024년 11월 05일 17:47:34
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -2,30 +2,30 @@ class MinHeap {
   constructor() {
     this.heap = [];
   }
-
   size() {
     return this.heap.length;
   }
-
-  swap(idx1, idx2) {
-    [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+  peek() {
+    return this.heap[0];
   }
-  add(value) {
-    this.heap.push(value);
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+  push(v) {
+    this.heap.push(v);
     this.bubbleUp();
   }
-
   bubbleUp() {
     let currIdx = this.heap.length - 1;
-    let parentIdx = Math.floor((currIdx - 1) / 2);
-    while (this.heap[parentIdx] && this.heap[parentIdx] > this.heap[currIdx]) {
-      this.swap(parentIdx, currIdx);
-      currIdx = parentIdx;
-      parentIdx = Math.floor((currIdx - 1) / 2);
+    let parIdx = Math.floor((currIdx - 1) / 2);
+
+    while (this.heap[parIdx] && this.heap[currIdx] < this.heap[parIdx]) {
+      this.swap(currIdx, parIdx);
+      currIdx = parIdx;
+      parIdx = Math.floor((currIdx - 1) / 2);
     }
   }
-
-  remove() {
+  pop() {
     if (this.heap.length === 1) {
       return this.heap.pop();
     }
@@ -34,50 +34,44 @@ class MinHeap {
     this.bubbleDown();
     return value;
   }
-
   bubbleDown() {
     let currIdx = 0;
-    let lIdx = currIdx * 2 + 1;
-    let rIdx = currIdx * 2 + 2;
-
+    let leftIdx = currIdx * 2 + 1;
+    let rightIdx = currIdx * 2 + 2;
+    const last = this.heap.length - 1;
     while (
-      (this.heap[lIdx] && this.heap[lIdx] < this.heap[currIdx]) ||
-      (this.heap[rIdx] && this.heap[rIdx] < this.heap[currIdx])
+      (this.heap[leftIdx] && this.heap[leftIdx] < this.heap[currIdx]) ||
+      (this.heap[rightIdx] && this.heap[rightIdx] < this.heap[currIdx])
     ) {
-      let smallerIdx = lIdx;
-      if (this.heap[rIdx] && this.heap[rIdx] < this.heap[smallerIdx]) {
-        smallerIdx = rIdx;
+      let minIdx = leftIdx;
+      if (this.heap[rightIdx] && this.heap[rightIdx] < this.heap[leftIdx]) {
+        minIdx = rightIdx;
       }
-      this.swap(currIdx, smallerIdx);
-      currIdx = smallerIdx;
-      lIdx = currIdx * 2 + 1;
-      rIdx = currIdx * 2 + 2;
+      this.swap(currIdx, minIdx);
+      currIdx = minIdx;
+      leftIdx = currIdx * 2 + 1;
+      rightIdx = currIdx * 2 + 2;
     }
   }
 }
 
 function solution(scoville, K) {
-  let answer = 0;
   const heap = new MinHeap();
-
-  for (let s of scoville) {
-    heap.add(s);
+  for (const s of scoville) {
+    heap.push(s);
   }
-
+  let count = 0;
   while (heap.size() >= 2) {
-    const first = heap.remove();
-    if (first >= K) {
-      return answer;
-    }
-    const second = heap.remove();
-    const mixed = first + second * 2;
-    heap.add(mixed);
-    answer++;
+    if (heap.peek() >= K) return count;
+    const fst = heap.pop();
+    const sec = heap.pop();
+    heap.push(fst + sec * 2);
+    count++;
   }
-
-  if (heap.remove() >= K) {
-    return answer;
-  } else {
-    return -1;
+  if (heap.peek() >= K) {
+    return count;
   }
+  return -1;
 }
+
+console.log(solution([1, 12, 3, 9, 2, 10], 7));
