8faf2e653e7817a1e0d441ce3644c2c55d3725b1
Sonjieun <57988006+handje@users.noreply.github.com>
2024-11-06 16:37:03 +0900

    [level 1] Title: 모의고사, Time: 4.44 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,21 +1,24 @@
 function solution(answers) {
-  const fst = [1, 2, 3, 4, 5];
-  const sec = [2, 1, 2, 3, 2, 4, 2, 5];
-  const trd = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  let fs = 0;
-  let ss = 0;
-  let ts = 0;
-
+  const answer = [];
+  const correct = [0, 0, 0];
+  const way = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
   for (let i = 0; i < answers.length; i++) {
-    if (fst[i % 5] === answers[i]) fs++;
-    if (sec[i % 8] === answers[i]) ss++;
-    if (trd[i % 10] === answers[i]) ts++;
+    for (let j = 0; j < way.length; j++) {
+      const arr = way[j];
+      if (arr[i % arr.length] === answers[i]) {
+        correct[j]++;
+      }
+    }
   }
 
-  const maxScore = Math.max(fs, ss, ts);
-  let answer = [];
-  if (fs === maxScore) answer.push(1);
-  if (ss === maxScore) answer.push(2);
-  if (ts === maxScore) answer.push(3);
+  const maxNum = Math.max(...correct);
+
+  for (let i = 0; i < correct.length; i++) {
+    if (correct[i] === maxNum) answer.push(i + 1);
+  }
   return answer;
 }
