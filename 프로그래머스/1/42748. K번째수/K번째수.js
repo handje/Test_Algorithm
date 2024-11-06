85d505b6cf2db65b78e8f98e0706b4de34733ad4
Sonjieun <57988006+handje@users.noreply.github.com>
2024-11-06 16:03:26 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,13 +1,10 @@
-//배열을 자르고, 정렬하고, k번째수를 리턴
-//100이하니까 내장함수
-
 function solution(array, commands) {
-  const answer = [];
-  for (let command of commands) {
-    const [i, j, k] = command;
-    const temp = array.slice(i - 1, j);
-    temp.sort((a, b) => a - b);
-    answer.push(temp[k - 1]);
-  }
-  return answer;
+    var answer = [];
+    for (let c of commands){
+        const [i,j,k]=c;
+        const num=array.slice(i-1,j);
+        num.sort((a,b)=>a-b)
+        answer.push(num[k-1])
+    }
+    return answer;
 }
