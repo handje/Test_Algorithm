c835ba9883180293f0e5ada117374fc1ca877df1
Sonjieun <57988006+handje@users.noreply.github.com>
2025-01-14 19:34:02 +0900

    [level 2] Title: 구명보트, Time: 35.04 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 38.1 MB, 시간: 37.12 ms
+메모리: 37.9 MB, 시간: 35.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 25일 15:56:44
+2025년 01월 14일 19:33:59
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,20 +1,18 @@
 function solution(people, limit) {
-  people.sort((a, b) => a - b);
-  let l = 0;
-  let r = people.length - 1;
+  people.sort((a, b) => b - a);
+  console.log(people);
+  let first = 0;
+  let last = people.length - 1;
   let count = 0;
-  while (l <= r) {
-    if (l === r) {
-      count++;
-      break;
-    }
-    if (people[l] + people[r] <= limit) {
-      l++;
-      r--;
+  while (first < last) {
+    if (people[first] + people[last] <= limit) {
+      first += 1;
+      last -= 1;
     } else {
-      r--;
+      first += 1;
     }
     count++;
+    if (first === last) count++;
   }
   return count;
 }
