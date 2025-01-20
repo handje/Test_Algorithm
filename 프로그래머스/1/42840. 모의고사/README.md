17711fb78fd3f23e9b53e22ef857328d6e52757e
Sonjieun <57988006+handje@users.noreply.github.com>
2025-01-20 18:21:42 +0900

    [level 1] Title: 모의고사, Time: 5.67 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 36.6 MB, 시간: 4.44 ms
+메모리: 36.8 MB, 시간: 5.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 06일 16:37:01
+2025년 01월 20일 18:21:45
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,24 +1,29 @@
 function solution(answers) {
-  const answer = [];
-  const correct = [0, 0, 0];
-  const way = [
-    [1, 2, 3, 4, 5],
-    [2, 1, 2, 3, 2, 4, 2, 5],
-    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-  ];
-  for (let i = 0; i < answers.length; i++) {
-    for (let j = 0; j < way.length; j++) {
-      const arr = way[j];
-      if (arr[i % arr.length] === answers[i]) {
-        correct[j]++;
-      }
+    const one=[1,2,3,4,5];
+    const two=[2,1,2,3,2,4,2,5];
+    const three=[3,3,1,1,2,2,4,4,5,5];
+    
+    const answer=[];
+    let count=[0,0,0]
+    
+    for(let i=0;i<answers.length;i++){
+        const correct=answers[i]
+        if(one[i%one.length]===correct){
+            count[0]++;
+        }
+                if(two[i%two.length]===correct){
+            count[1]++;
+        }
+                if(three[i%three.length]===correct){
+            count[2]++;
+        }
     }
-  }
+    
+    const big=Math.max(...count);
+    
+    for(let i=0;i<3;i++){
+        if(count[i]===big)answer.push(i+1)
+    }
+   return answer
 
-  const maxNum = Math.max(...correct);
-
-  for (let i = 0; i < correct.length; i++) {
-    if (correct[i] === maxNum) answer.push(i + 1);
-  }
-  return answer;
 }
