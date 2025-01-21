ef80e98d1a52abdd732c3714dfab2588a373252f
Sonjieun <57988006+handje@users.noreply.github.com>
2025-01-21 23:01:46 +0900

    [level 1] Title: 폰켓몬, Time: 0.58 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,8 +1,7 @@
 function solution(nums) {
-  const set = new Set(nums);
-  if (nums.length / 2 < set.size) {
-    return nums.length / 2;
-  } else {
-    return set.size;
-  }
+    var answer = 0;
+    const setNums=new Set(nums);
+    const count=nums.length/2
+    if(setNums.size>count)return count;
+    else return setNums.size
 }
