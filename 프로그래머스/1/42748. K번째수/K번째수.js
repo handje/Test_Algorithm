872c911dbe9d394ccad387f0f42d08c49c0d78dd
Sonjieun <57988006+handje@users.noreply.github.com>
2025-01-23 23:17:36 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,8 @@
 function solution(array, commands) {
-    var answer = [];
-    for (let c of commands){
-        const [i,j,k]=c;
-        const num=array.slice(i-1,j);
-        num.sort((a,b)=>a-b)
-        answer.push(num[k-1])
-    }
+    const answer = [];
+    for (let [i,j,k] of commands){
+const newArr=array.slice(i-1,j).sort((a,b)=>a-b)
+answer.push(newArr[k-1])
+}
     return answer;
 }
