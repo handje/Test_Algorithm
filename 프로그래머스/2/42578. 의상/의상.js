4cb421ef83c5071a4f624f66701b637f63f7943f
Sonjieun <57988006+handje@users.noreply.github.com>
2025-01-24 11:38:22 +0900

    [level 2] Title: 의상, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,17 +1,15 @@
 function solution(clothes) {
-  const map = new Map();
-  for (let [name, cate] of clothes) {
-    if (map.has(cate)) {
-      map.set(cate, [...map.get(cate), name]);
-    } else {
-      map.set(cate, [name]);
+    const map=new Map();
+    
+    for(let [a,b] of clothes){
+        const curr=map.get(b)??0
+        map.set(b,curr+1)
     }
-  }
 
-  let count = 1;
-  for (let value of map.values()) {
-    count *= value.length + 1;
-  }
-
-  return count - 1;
+    let answer=1;
+    
+    for(let [key,value] of map.entries()){
+        answer*=value+1
+    }
+    return answer-1
 }
