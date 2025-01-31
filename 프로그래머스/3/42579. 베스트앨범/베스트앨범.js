3ab1fd774fa6686d8c7ebea617c6fc970d4c45c1
Sonjieun <57988006+handje@users.noreply.github.com>
2025-01-31 23:56:34 +0900

    [level 3] Title: 베스트앨범, Time: 4.60 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,39 +1,24 @@
 function solution(genres, plays) {
-    const answer=[]
-    const genreCount=new Map();
-    const music=new Map();
-    
-    for(let i=0;i<genres.length;i++){
-        const gr=genres[i];
-        
-        if(genreCount.has(gr)){
-            genreCount.set(gr,genreCount.get(gr)+plays[i])
-        }else{
-            genreCount.set(gr,plays[i])
-        }
-        
-        if(music.has(gr)){
-            music.set(gr,[...music.get(gr),[i,plays[i]]])
-        }else{
-            music.set(gr,[[i,plays[i]]])
-        }
-    }
-    
-    const sortedGenre=Array.from(genreCount.entries()).sort((a,b)=>b[1]-a[1])
-    
-    for(let [key,values] of music){
-        const sorted=Array.from(values).sort((a,b)=>b[1]-a[1]);
-        music.set(key,sorted)
-    }
+  const genreMap = new Map();
 
-    for(let g of sortedGenre){
-        const temp=music.get(g[0]);  
-        if(temp.length>=2){
-            answer.push(temp[0][0]);
-         answer.push(temp[1][0]);
-        }else{
-            answer.push(temp[0][0])
-        }
+  genres.forEach((genre, index) => {
+    if (!genreMap.has(genre)) {
+      genreMap.set(genre, { totalPlays: 0, songs: [] });
     }
-    return answer
+    genreMap.get(genre).totalPlays += plays[index];
+    genreMap.get(genre).songs.push({ play: plays[index], index });
+      
+  });
+
+  const sortedGenres = [...genreMap.entries()].sort((a, b) => b[1].totalPlays - a[1].totalPlays);
+  console.log(sortedGenres)
+  const result = [];
+  for (const [genre, info] of sortedGenres) {
+    const topSongs = info.songs
+      .sort((a, b) => b.play - a.play || a.index - b.index)
+      .slice(0, 2);
+    topSongs.forEach(song => result.push(song.index));
+  }
+  
+  return result;
 }
