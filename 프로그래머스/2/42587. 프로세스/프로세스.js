8083de1208623663f744a8da637db19d107ed53d
Sonjieun <57988006+handje@users.noreply.github.com>
2025-02-03 20:12:02 +0900

    [level 2] Title: 프로세스, Time: 0.52 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,18 +1,23 @@
 function solution(priorities, location) {
-    const prior=[];
-    const answer=[];
-    for(let i=0; i<priorities.length;i++){
-        prior.push([i,priorities[i]])
+    let count=1;
+    const queue=[];
+    for(let i=0;i<priorities.length;i++){
+        const value=priorities[i]
+        queue.push([i,value])
     }
 
-    while(prior.length>0){
-        let [idx,p]=prior.shift();
-        let maxNum=Math.max(...prior.map((v)=>v[1]));
-        if(p>=maxNum){
-            answer.push(idx)
-        }else{
-            prior.push([idx,p])
+    while(priorities.length>0){
+        const [idx,value]=queue.shift();
+        const curr=priorities.shift()
+
+        if(Math.max(...priorities)>curr){
+            priorities.push(curr);
+            queue.push([idx,value])
+            continue;
         }
+        if(idx===location){
+            return count;
+        }
+        count++;
     }
-    return answer.findIndex((e)=>e===location)+1
 }
