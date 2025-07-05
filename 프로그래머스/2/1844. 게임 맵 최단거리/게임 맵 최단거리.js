50f014ec5bd2bd829025af548302d9b5c3944374
Sonjieun <57988006+handje@users.noreply.github.com>
2025-07-05 22:35:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 30.79 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,38 +1,30 @@
 function solution(maps) {
-    const n=maps[0].length-1;
-    const m=maps.length-1;
-    
-    const dx=[0,0,1,-1];
-    const dy=[1,-1,0,0];
-    
-    const q=[[0,0]];
-    
-    while(q.length){
-        const [x,y]=q.shift();
-        
-        for(let i=0;i<4;i++){
-            const newX=x+dx[i];
-            const newY=y+dy[i];
-            
-            if(newX===m&&newY===n){
-                return maps[x][y]+1
-            }
-            if(newX<0 || newX>m){
-                continue;
-            }
-            if(newY<0||newY>n){
-                continue;
-            }
-            if(maps[newX][newY]===0 || maps[newX][newY]>1){
-                continue;
-            }
-            maps[newX][newY]=maps[x][y]+1;
-            q.push([newX,newY])
-        }
-        
+  const queue = [];
+
+  const n = maps.length; //행(y)개수
+  const m = maps[0].length; //열(x) 개수
+
+  const dx = [1, -1, 0, 0];
+  const dy = [0, 0, 1, -1];
+
+  queue.push([0, 0]);
+  while (queue.length > 0) {
+    const [y, x] = queue.shift();
+
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue; //범위밖
+
+      if (maps[ny][nx] !== 1) continue; //벽 or 이미 방문
+
+      maps[ny][nx] += maps[y][x];
+      queue.push([ny, nx]);
     }
-    if(maps[m][n]>1){
-        return maps[m][n]
-    }
+  }
+  if (maps[n - 1][m - 1] === 1) {
     return -1;
+  }
+  return maps[n - 1][m - 1];
 }
