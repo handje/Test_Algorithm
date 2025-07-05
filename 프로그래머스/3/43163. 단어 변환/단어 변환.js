96101f107fd198e77f2c1fe0e809eddb6cffbaad
Sonjieun <57988006+handje@users.noreply.github.com>
2025-07-05 23:14:15 +0900

    [level 3] Title: 단어 변환, Time: 21.28 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,43 +1,53 @@
-function check(prev,curr){
-    let count=0
-    for(let i=0;i<curr.length;i++){
-        if(prev[i]!==curr[i]){
-            count++;
-        }
-        if(count>1){
-            return false;
-        }
-    }
-    return true;
-}
-
 function solution(begin, target, words) {
-    if(words.indexOf(target)===-1){
-        return 0;
+  //한개차이 함수
+  const find = (a, b) => {
+    let diff = 0;
+    for (let i = 0; i < a.length; i++) {
+      const curr = a.charCodeAt(i); //문자열의 인덱스로 아스키코드 변환
+      const next = b.charCodeAt(i);
+
+      if (curr - next !== 0) diff++;
     }
-    
-    const queue=[[begin,0]];
-    
-    const visited=new Map();
-    
-    let answer = 0;
-    
-    while(queue.length>0){
-        console.log(queue)
-        let [word,cnt]=queue.shift();
-        visited.set(word,cnt);
-        if(word===target){
-            return cnt;
-        }
-        for(let i=0;i<words.length;i++){
-            if(visited.has(words[i])){
-                continue;
-            }
-            if(check(word,words[i])){
-                queue.push([words[i],cnt+1]);
-            }
-        }
-        
+    return diff === 1 ? true : false;
+  };
+
+  //한개차이 저장
+  const map = new Map();
+  for (let i = 0; i < words.length; i++) {
+    const temp = [];
+    for (let j = 0; j < words.length; j++) {
+      if (i === j) continue;
+      if (find(words[i], words[j])) {
+        temp.push(words[j]);
+      }
     }
-    return answer;
+    map.set(words[i], temp);
+  }
+
+  //begin도 포함하기(words에 없을 수 있음)
+  const fromBegin = [];
+  for (let i = 0; i < words.length; i++) {
+    if (find(begin, words[i])) {
+      fromBegin.push(words[i]);
+    }
+  }
+  map.set(begin, fromBegin);
+
+  //bfs
+  const queue = [[begin, 0]];
+  const visited = new Set();
+  visited.add(begin);
+  while (queue.length > 0) {
+    const [curr, cnt] = queue.shift();
+    if (curr === target) {
+      return cnt;
+    }
+    for (let next of map.get(curr) || []) {
+      if (!visited.has(next)) {
+        queue.push([next, cnt + 1]);
+        visited.add(next);
+      }
+    }
+  }
+  return 0;
 }
